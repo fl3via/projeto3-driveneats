5dc1c0d8257df5798c3a04843a84d54a01c6713d
Flavia Mendes <flaviamendesdasilva99@gmail.com>
2023-01-07 20:00:48 -0300

    add function send request in whatsapp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function getPrato(prato) {
     const selecionado = document.querySelector("#prato .green-border")
-    if(selecionado !== null){
+    if (selecionado !== null) {
         selecionado.classList.remove("green-border")
     }
     prato.classList.add("green-border")
@@ -10,7 +10,7 @@ function getPrato(prato) {
 
 function getBebida(bebida) {
     const selecionado = document.querySelector("#bebida .green-border")
-    if(selecionado !== null){
+    if (selecionado !== null) {
         selecionado.classList.remove("green-border")
     }
     bebida.classList.add("green-border")
@@ -19,8 +19,8 @@ function getBebida(bebida) {
 
 
 function getSobremesa(sobremesa) {
-    const selecionado =  document.querySelector("#sobremesa .green-border")
-    if(selecionado !== null){
+    const selecionado = document.querySelector("#sobremesa .green-border")
+    if (selecionado !== null) {
         selecionado.classList.remove("green-border")
     }
     sobremesa.classList.add("green-border")
@@ -28,12 +28,38 @@ function getSobremesa(sobremesa) {
 }
 
 
-function onButton(){
+function onButton() {
     let quantidade = document.getElementsByClassName("green-border").length
-    if(quantidade === 3){
+    if (quantidade === 3) {
         document.querySelector("button").classList.add("botao-enviar")
         document.querySelector("button p").classList.add("fechar-pedido")
         document.querySelector("button p").innerHTML = "Fechar pedido"
         document.querySelector("button").removeAttribute("disabled")
     }
 }
+
+function enviarPedido() {
+        const prato = document.querySelector("#prato .green-border h4").innerHTML;
+        const bebida = document.querySelector("#bebida .green-border h4").innerHTML;
+        const sobremesa = document.querySelector("#sobremesa .green-border h4").innerHTML;
+      
+        const precoPrato = document.querySelector("#prato .green-border .preco").innerHTML;
+        const precoBebida = document.querySelector("#bebida .green-border .preco").innerHTML;
+        const precoSobremesa = document.querySelector("#sobremesa .green-border .preco" ).innerHTML;
+      
+        const precoPratoNumber = valores(precoPrato);
+        const precoBebidaNumber = valores(precoBebida);
+        const precoSobremesaNumber = valores(precoSobremesa);
+        const precoTotal = precoPratoNumber + precoBebidaNumber + precoSobremesaNumber;
+      
+        const mensagem = `Olá, gostaria de fazer o pedido: \n- Prato: ${prato} \n- Bebida: ${bebida} \n- Sobremesa: ${sobremesa} \nTotal: R$ ${precoTotal.toFixed(2)}`
+      
+        const whatsapp = "https://wa.me/5524981593428?text=" + encodeURI(mensagem);
+        window.open(whatsapp);
+      }
+      
+      function valores(string) {
+        let numero = string.replace(/[^0-9]/g, "");
+        return parseInt(numero) / 100;
+      }
+    
