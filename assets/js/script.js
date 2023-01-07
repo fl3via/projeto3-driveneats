b63b50ede1f7344548465c5f0db786d97a37bf91
Flavia Mendes <flaviamendesdasilva99@gmail.com>
2023-01-07 02:35:21 -0300

    add order button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,39 @@
 function getPrato(prato) {
-    const selecionado = document.querySelector("#pratos .green-border")
+    const selecionado = document.querySelector("#prato .green-border")
     if(selecionado !== null){
         selecionado.classList.remove("green-border")
     }
     prato.classList.add("green-border")
+    onButton()
 }
 
 
 function getBebida(bebida) {
-    const selecionado = document.querySelector("#bebidas .green-border")
+    const selecionado = document.querySelector("#bebida .green-border")
     if(selecionado !== null){
         selecionado.classList.remove("green-border")
     }
     bebida.classList.add("green-border")
+    onButton()
 }
 
 
 function getSobremesa(sobremesa) {
-    const selecionado =  document.querySelector("#sobremesas .green-border")
+    const selecionado =  document.querySelector("#sobremesa .green-border")
     if(selecionado !== null){
         selecionado.classList.remove("green-border")
     }
     sobremesa.classList.add("green-border")
+    onButton()
+}
+
+
+function onButton(){
+    let quantidade = document.getElementsByClassName("green-border").length
+    if(quantidade === 3){
+        document.querySelector("button").classList.add("botao-enviar")
+        document.querySelector("button p").classList.add("fechar-pedido")
+        document.querySelector("button p").innerHTML = "Fechar pedido"
+        document.querySelector("button").removeAttribute("disabled")
+    }
 }
